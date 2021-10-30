5fa78f07d34448876d3c2ec8c27f39f7eadc530d
Sasha Czerniawski <sashacandoit@gmail.com>
2021-10-30 17:04:31 -0400

    problems solved and jasmine tests all pass

--- find-findindex.js
+++ find-findindex.js
@@ -11,9 +11,16 @@ findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
 findUserByUsername(users, 'taco') // undefined
 */
 
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
 
 function findUserByUsername(usersArray, username) {
-
+  return usersArray.find(function(user) {
+    return user.username === username;
+  })
 }
 
 /*
@@ -29,4 +36,10 @@ removeUser(users, 'akagen') // {username: 'akagen'}
 removeUser(users, 'akagen') // undefined
 */
 
-function removeUser(usersArray, username) {}
+function removeUser(usersArray, username) {
+  let foundIndex = usersArray.findIndex(function(user) {
+    return user.username === username;
+  })
+  if(foundIndex === -1) return;
+  return usersArray.splice(foundIndex,1)[0];
+}
